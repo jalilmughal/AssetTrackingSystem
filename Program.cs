e86584a656bd035863bef233eacd6cc556536fab
Jalil Bahadori <info@meta-gits.dk>
2021-03-09 13:40:46 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssetTrackingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
