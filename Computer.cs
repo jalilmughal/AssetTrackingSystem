122a90fd72765bbec6b0017ae14ec99b0b7a8aa4
A.Jalil Bahadori <jalilmughal@live.com>
2021-03-12 03:02:36 +0100

    Classes for Assets, Mobiles, Computers,WriterExtension Added in Program.cs
    Accepts and print all values and adds to computerAssets-List if Category-input is laptop else adds to mobileAssets-List.

---
+++ Computer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssetTrackingSystem
+{
+    public class Computer
+    {
+        public Computer()
+        {
+
+        }
+        public Computer(string purchaseDate, string price, string modelName)
+        {
+            PurchaseDate = purchaseDate;
+            Price = price;
+            ModelName = modelName;
+        }
+
+        public string PurchaseDate { get; set; }
+        public string Price { get; set; }
+        public string ModelName { get; set; }
+    }
+}
